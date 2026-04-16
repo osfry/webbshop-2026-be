17da0aa037ba34552c6243675613646c4f095b79
lukasdannemann <lukasdannemann@outlook.com>
2026-04-16 09:21:46 +0200

    add origin to cors

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(async (req, res, next) => {
   }
 });
 app.use(cors({
-    origin: "http://localhost:5500", 
+    origin: ["http://localhost:5500", "http://127.0.0.1:5500"], 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"] 
 }));
